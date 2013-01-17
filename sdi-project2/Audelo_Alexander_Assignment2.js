c48646d38ea7c5755937c37ff8a44bc26f99f1e7
Alexander Audelo <audelo06@yahoo.com>
2013-01-17 18:03:31 -0500

    Added functions

--- sdi-project2/Audelo_Alexander_Assignment2.js
+++ sdi-project2/Audelo_Alexander_Assignment2.js
@@ -18,4 +18,43 @@ var animals = [
 	hoursSlept = 8,
 	wellSlept = true;
 	
-	
+	function set hoursSlept(wellSlept) {
+		if ( wellSlept ) {
+			housSlept = 8;
+	    }
+		else {
+			hourslept = 5;
+	    }
+}
+
+// Number Function
+function wakeUp ( hoursSlept) {
+	if ( hoursSlept >= 8) 
+	{
+	return "Well rested for the Zoo!";
+	}
+	else {
+	return "It's going to be a long day because I did not get enough sleep.";
+	}
+}
+
+// Array Function
+function goSeeingAnimal ( theAnimals, facesmade ) {
+var seeingAnimals = new Array ( theAnimals.length );
+for ( var i = 0; i < theAnimals.length; i+++ ) {
+seeingAnimals [i] = "When we saw the" + theAnimals[i]
+"we" + " " + facesMade[i];
+}
+return seeingAnimals
+}
+
+// Boolean Function
+funcion question ( theAnimals ) {
+if ( animals.indexOf ('Tiger') >= 0) {
+return "Dad asked us if we got to see the tigers and we answered, we saw the tigers and we were scared of them.";
+}
+else {
+return "Dad asked us if we got to see the tigers and we answered, we wanted to see the tigers but were unable.";
+}
+}
+
