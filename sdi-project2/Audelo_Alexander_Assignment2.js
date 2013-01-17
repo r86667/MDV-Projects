9dd80f3b697af0e7b466806cf66d2a69e0af46e5
Alexander Audelo <audelo06@yahoo.com>
2013-01-17 17:21:43 -0500

    Added variables

---
+++ sdi-project2/Audelo_Alexander_Assignment2.js
@@ -0,0 +1,21 @@
+// alert("JavaScript works!");
+
+// Alexander Audelo
+// SDI Project 2
+// 1/17/2013
+// Visiting the Zoo
+
+
+var animals = [
+	"Elephant",
+	"Tiger",
+	"Monkey",
+	"Girrafe"
+	],
+	goingToTheZoo = "We are going to the zoo today!",
+	faces = ["stuck out our tongue.", "started to laugh.", "bug eyed looked at them.", "wanted to ride them."],
+	leavingTheZoo = "Nooo we don't want to leave",
+	hoursSlept = 8,
+	wellSlept = true;
+	
+	
