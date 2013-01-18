22307612a6b681cb0a3333d2d57f124aefdab4e2
Alexander Audelo <audelo06@yahoo.com>
2013-01-17 20:21:20 -0500

    Added more functions

--- sdi-project2/Audelo_Alexander_Assignment2.js
+++ sdi-project2/Audelo_Alexander_Assignment2.js
@@ -18,43 +18,58 @@ var animals = [
 	hoursSlept = 8,
 	wellSlept = true;
 	
-	function set hoursSlept(wellSlept) {
+	function setHoursSlept(wellSlept) {
 		if ( wellSlept ) {
 			housSlept = 8;
-	    }
-		else {
-			hourslept = 5;
+	    } else {
+			hoursSlept = 5;
 	    }
 }
 
 // Number Function
-function wakeUp ( hoursSlept) {
-	if ( hoursSlept >= 8) 
-	{
+function wakeUp(hoursSlept) {
+	if (hoursSlept >= 8) {
 	return "Well rested for the Zoo!";
-	}
-	else {
+	} else {
 	return "It's going to be a long day because I did not get enough sleep.";
 	}
 }
 
 // Array Function
-function goSeeingAnimal ( theAnimals, facesmade ) {
-var seeingAnimals = new Array ( theAnimals.length );
-for ( var i = 0; i < theAnimals.length; i+++ ) {
-seeingAnimals [i] = "When we saw the" + theAnimals[i]
-"we" + " " + facesMade[i];
+function goSeeingAnimal (theAnimals, facesMade) {
+var seeingAnimals = new Array (theAnimals.length);
+for (var i = 0; i < theAnimals.length; i++) {
+seeingAnimals[i] = "When we saw the " + theAnimals[i] + " " + "we" + " " + facesMade[i];
 }
 return seeingAnimals
 }
 
 // Boolean Function
-funcion question ( theAnimals ) {
-if ( animals.indexOf ('Tiger') >= 0) {
+function question(theAnimals) {
+if (animals.indexOf('Tiger') >= 0){
 return "Dad asked us if we got to see the tigers and we answered, we saw the tigers and we were scared of them.";
-}
-else {
+} else {
 return "Dad asked us if we got to see the tigers and we answered, we wanted to see the tigers but were unable.";
 }
 }
 
+// String Function
+function myfunction(excitment){
+var concatination = excitment;
+if (excitment === "Yay! we are going to the zoo!"){
+	concatination += "can't wait to get there!";
+	} else { 
+	concatination += ", we want to see more animals.";
+}
+return concatination;
+}
+
+setHoursSlept(wellSlept);
+console.log(wakeUp(hoursSlept));
+console.log(myfunction("Yay! we are going to the zoo!"));
+animalsWeSaw = goSeeingAnimal(animals, faces);
+for ( var x in animalsWeSaw ) {
+console.log(animalsWeSaw[x]);
+}
+console.log(myfunction(leavingTheZoo));
+console.log(question(animals));
