12509ec82f954a136caffb3fcf2bba46d7da7e4c
Alexander Audelo <audelo06@yahoo.com>
2013-01-24 21:19:30 -0500

    Added Methods and Strings

--- sdi-project3/Audelo_Alexander_Assignment3.js
+++ sdi-project3/Audelo_Alexander_Assignment3.js
@@ -79,3 +79,62 @@ var vacation = {
 	    vacation.narration("We did not purchase our tickets yet. We are comparing a few more airlines!");
 	};
     },
+            // Method: The Accessor
+    getData: function(json) {
+	var i = 0;
+	while (i<json.tickets.length){
+            
+	    var personTraveling = json.tickets[i];
+	    vacation.narration("Ticket Number " + personTraveling.tNum + ", for " + personTraveling.name + ", on flight number " + personTraveling.fNum);
+	    i++;
+	};
+	return json;
+    },
+    boardingGroup: function() {
+	for (var bg=0; bg < receivedjson["tickets"].length; bg++) {
+	    var name = receivedjson["tickets"][bg].name;
+            var bGroup = receivedjson["tickets"][bg].bGroup;
+        //String Return
+	    vacation.narration("" + name + "'s boarding group is " + bGroup + ""); 
+	}
+    },
+
+    // Mutator Method
+    ticketCost: function(name, tCost){
+	var travelCost = [];
+	var actualTicket = function(ticket) {
+	    if (ticket == tCost) {
+		travelCost.push(ticket);
+                vacation.narration(travel);
+            } else {
+	    vacation.narration("This is not the price of the ticket we purchased.");
+	    }
+	}
+	var purchasedTicketCosts = (" " + name + " total ticket cost was " + tCost+ ".");
+	    vacation.narration(purchasedTicketCosts);
+    },
+    // Method FUNction
+    startTravel: function(layover) {
+        layovers = 1;
+	var layovers = 3;
+	for (var layoverTotal = 1; layoverTotal < layovers; layoverTotal ++) {
+            vacation.narration("We have stopped at layover " + layoverTotal + " of 3");
+	    if (layovers == 3) {
+                 var layoversLeft = layovers - layoverTotal; //Math
+
+	    vacation.narration(+layoversLeft + " stops to go before we land in " + vacationDest + "!");
+		};
+	    }
+	    vacation.narration("We have finally arrived at " +vacationDest + " lets get this vacation going!");
+
+    },
+        ticketPrice: function() {
+	for (var tp=0; tp < receivedjson["tickets"].length; tp++) {
+	    var name = receivedjson["tickets"][tp].name;
+            var tCost = receivedjson["tickets"][tp].tCost;
+        //String Return
+	    vacation.narration("" + name + "'s ticket cost was " + tCost + ""); 
+	}
+    }
+};
+
