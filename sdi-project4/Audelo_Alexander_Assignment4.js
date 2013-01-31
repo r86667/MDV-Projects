fcbd4def26edeedbc1ddbd6c0b76fe61f908b056
Alexander Audelo <audelo06@yahoo.com>
2013-01-30 22:44:01 -0500

    Problem 1

--- sdi-project4/Audelo_Alexander_Assignment4.js
+++ sdi-project4/Audelo_Alexander_Assignment4.js
@@ -6,3 +6,25 @@
 // Project 4: Library
 
 
+// Problem 1: Find the smallest value in an array that is greater than a given number
+
+var smallValue = function(numArray, number) {
+	var largerNums = [],
+		smallestNum;
+
+	for (i = 0; i < numArray.length; i++) {
+		if (numArray[i] > number) {
+			largerNums.push(numArray[i]);
+		}
+	}
+
+	smallestNum = largerNums[0];
+
+	for (j = 0; j < largerNums.length; j++) {
+		if (smallestNum > largerNums[j]) {
+			smallestNum = largerNums[j];
+		}
+	}
+
+	return ("The smallest number that is greater than " + number + " is " + smallestNum + ".");
+}
