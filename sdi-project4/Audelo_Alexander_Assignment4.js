d8f182592b33b36c014a9d8e6211b61771f38a28
Alexander Audelo <audelo06@yahoo.com>
2013-01-30 22:47:45 -0500

    Problem 2 & 3 added

--- sdi-project4/Audelo_Alexander_Assignment4.js
+++ sdi-project4/Audelo_Alexander_Assignment4.js
@@ -28,3 +28,26 @@ var smallValue = function(numArray, number) {
 
 	return ("The smallest number that is greater than " + number + " is " + smallestNum + ".");
 }
+
+// Problem 2: Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+
+
+var specificDec = function (number) {
+	var newNumber;
+
+	newNumber = number.toFixed(2);
+
+	return (number + " has been rounded to " + newNumber);	
+}
+
+// Problem 3: Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+
+var charReplace = function (string, oldChar, newChar) {
+	var myString = string,
+		old = new RegExp(oldChar, "g");
+
+	myString = myString.replace(old, newChar);
+
+	return myString;
+}
+
